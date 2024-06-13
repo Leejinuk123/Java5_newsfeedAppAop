2ea547539eb7ced47c9ac576287de8a00043c834
leejinuk <leejinouk123@kakao.com>
2024-06-13 15:48:21 +0900

    AllArgumentsConstruct 추가

--- src/main/java/com/sparta/newsfeedapp/dto/user/LoginResponseDto.java
+++ src/main/java/com/sparta/newsfeedapp/dto/user/LoginResponseDto.java
@@ -1,14 +1,16 @@
 package com.sparta.newsfeedapp.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginResponseDto {
     private String token;
     private String refreshToken;
 
-    public LoginResponseDto(String token, String refreshToken) {
-        this.token = token;
-        this.refreshToken = refreshToken;
-    }
+//    public LoginResponseDto(String token, String refreshToken) {
+//        this.token = token;
+//        this.refreshToken = refreshToken;
+//    }
 }
--- src/main/java/com/sparta/newsfeedapp/dto/user/ProfileResponseDto.java
+++ src/main/java/com/sparta/newsfeedapp/dto/user/ProfileResponseDto.java
@@ -1,9 +1,11 @@
 package com.sparta.newsfeedapp.dto.user;
 
 import com.sparta.newsfeedapp.entity.UserStatusEnum;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ProfileResponseDto {
     private String userId;
     private String name;
@@ -11,11 +13,11 @@ public class ProfileResponseDto {
     private String email;
     private UserStatusEnum userStatus;
 
-    public ProfileResponseDto(String userId, String name, String bio, String email, UserStatusEnum userStatus) {
-        this.userId = userId;
-        this.name = name;
-        this.bio = bio;
-        this.email = email;
-        this.userStatus = userStatus;
-    }
+//    public ProfileResponseDto(String userId, String name, String bio, String email, UserStatusEnum userStatus) {
+//        this.userId = userId;
+//        this.name = name;
+//        this.bio = bio;
+//        this.email = email;
+//        this.userStatus = userStatus;
+//    }
 }
