bcfb670c6e905db11f64e4b449df9411cca5b634
leejinuk <leejinouk123@kakao.com>
2024-06-13 11:54:25 +0900

    Dto 명 변경 (맨 앞글자 대문자로)

--- src/main/java/com/sparta/newsfeedapp/dto/user/DeleteRequestDto.java
+++ src/main/java/com/sparta/newsfeedapp/dto/user/DeleteRequestDto.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
-public class deleteRequestDto {
+public class DeleteRequestDto {
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()-_=+\\\\|\\[{\\]};:'\",<.>/?]).{10,}$", message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야 합니다.")
     @Size(min = 10, message = "비밀번호는 최소 10글자 이상이어야 합니다.")
     @NotBlank(message = "paaword는 필수입니다.")
--- src/main/java/com/sparta/newsfeedapp/dto/user/UpdateRequestDto.java
+++ src/main/java/com/sparta/newsfeedapp/dto/user/UpdateRequestDto.java
@@ -4,10 +4,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-public class updateRequestDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateRequestDto {
 
     @Size(max = 8, message = "이름은 최대 8자입니다.")
     private String name;
